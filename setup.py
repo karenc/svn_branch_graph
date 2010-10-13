cda1499f56161a813be25c2b24802c8b5130a1b9
karen chan <karen@isotoma.com>
2010-10-13 21:24:11 +0100

    Add svn_branch_graph package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name='svn_branch_graph',
+        version='0.1',
+        author='Karen Chan',
+        author_email='karen.chan@isotoma.com',
+        license='BSD',
+        packages=['svn_branch_graph'],
+        )
