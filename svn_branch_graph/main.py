25ab31e2eb46b0fd95069499a629ca7802a8dd43
karen chan <karen@isotoma.com>
2010-10-14 20:58:55 +0100

    Draw svn branch graph

--- svn_branch_graph/main.py
+++ svn_branch_graph/main.py
@@ -3,6 +3,7 @@
 import ConfigParser
 import json
 import os.path
+import re
 import subprocess
 import sys
 import xml.etree.ElementTree
@@ -17,10 +18,14 @@ def call(cmd):
 config = ConfigParser.RawConfigParser()
 config.read('svn_branch_graph.cfg')
 
-def get_branch_url():
-    svn_branches_url = '%s%s' % (config.get('svn', 'server-url'),
-            config.get('svn', 'branches-path', 'branches'))
+server_url = config.get('svn', 'server-url')
 
+branches_path = config.get('svn', 'branches-path')
+if not branches_path:
+    branches_path = 'branches'
+
+def get_branch_url():
+    svn_branches_url = '%s%s' % (server_url, branches_path)
     return svn_branches_url
 
 def get_svn_ls(svn_branches_url):
@@ -30,7 +35,7 @@ def get_svn_ls(svn_branches_url):
     return stdout.splitlines()
 
 def get_svn_log(svn_branch_url):
-    return_code, stdout = call(['svn', 'log', '--xml', '--stop-on-copy',
+    return_code, stdout = call(['svn', 'log', '-v', '--xml', '--stop-on-copy',
         svn_branch_url])
     if return_code != 0:
         raise RunTimeError('svn log %s failed.' % svn_branch_url)
@@ -38,11 +43,21 @@ def get_svn_log(svn_branch_url):
     svn_log = []
     for logentry in root.findall('logentry'):
         log = {}
-        log['revision'] = logentry.get('revision')
+        log['revision'] = int(logentry.get('revision'))
         log['author'] = logentry.findtext('author')
         log['msg'] = logentry.findtext('msg')
         svn_log.append(log)
-    return svn_log
+    copyfrom = None
+    for path in logentry.findall('paths/path'):
+        if path.get('copyfrom-rev'):
+            copyfrom_path = path.get('copyfrom-path')
+            copyfrom_rev = path.get('copyfrom-rev')
+            return_code, stdout = call(['svn', 'log', '--xml', '--limit=1',
+                '%s%s@%s' % (server_url, copyfrom_path, copyfrom_rev)])
+            log = xml.etree.ElementTree.fromstring(stdout)
+            copyfrom = int(log.find('logentry').get('revision'))
+    svn_log.reverse()
+    return svn_log, copyfrom
 
 BRANCH_COOKIE = 'svn-branches'
 
@@ -72,20 +87,31 @@ class GetGraph(object):
         :Parameters:
           - `branch`: list of branch names
         """
-        i = web.input(branch=[])
+        i = web.input(branch=[], branch_regexp='')
         # cookie lasts for a week
         web.setcookie('svn-branches', i.branch, 3600 * 24 * 7)
         svn_branch_url = get_branch_url()
-        svn_logs = {}
-        revisions = []
-        for branch in i.branch:
-            svn_logs[branch] = get_svn_log('/'.join([svn_branch_url, branch]))
-            revisions.extend([x['revision'] for x in svn_logs[branch]])
-        revisions.sort()
+
+        if i.branch_regexp:
+            pattern = re.compile(i.branch_regexp)
+            branches = get_svn_ls(svn_branch_url)
+            branches = [b.strip('/') for b in branches if pattern.match(b)]
+        else:
+            branches = i.branch
+
+        svn_logs = []
+        copyfromlist = {}
+        for branch in branches:
+            logs, copyfrom = get_svn_log('/'.join([svn_branch_url, branch]))
+            copyfromlist[branch] = copyfrom
+            for log in logs:
+                log.setdefault('branch', branch)
+            svn_logs.extend(logs)
+        svn_logs.sort(lambda a, b: cmp(a['revision'], b['revision']))
         return render.graph(
-                i.branch,
-                json.dumps(svn_logs),
-                json.dumps(revisions),
+                branches,
+                json.dumps(svn_logs).replace('\\', '\\\\'),
+                json.dumps(copyfromlist).replace('\\', '\\\\'),
                 )
 
 
